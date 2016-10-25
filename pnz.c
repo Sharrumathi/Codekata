5898cbdff97415b94def3c38aebf2e0002ec0dfa
Sharrumathi <Sharrumathi@users.noreply.github.com>
2016-10-25 11:14:20 +0530

    Update pnz.c

--- pnz.c
+++ pnz.c
@@ -7,7 +7,7 @@ clrscr();
 printf("\nEnter your number:");
 scanf("%d",&num);
 if(num>0)
-printf("\%d is positive",num);
+printf("\n%d is positive",num);
 else if(num<0)
 printf("\n%d is negative",num);
 else

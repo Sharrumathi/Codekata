89d6f638c3658fc0b54b50ded5c679c1707dd07a
Sharrumathi <Sharrumathi@users.noreply.github.com>
2016-10-25 14:19:23 +0530

    Create hel.c

---
+++ hel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+string s;
+clrscr();
+printf("\n Enter the String:");
+gets(s);
+for(i=1;i>=5;i++)
+printf("Hello");
+getch();
+}

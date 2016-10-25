371229a5f4711630538bcdbe924d775d15f47f58
Sharrumathi <Sharrumathi@users.noreply.github.com>
2016-10-25 11:53:42 +0530

    Create arn.c

---
+++ arn.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+clrscr();
+printf("\n Enter your character:");
+scanf("%c",&c);
+if( (c>='a'&&c<='z') || (c>='A'&&c<='Z') )
+printf("\n %c is alphabet",c);
+else
+printf("\n %c is not a alphabet",c);
+getch();
+}
